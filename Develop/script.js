6dd3b4c8e071abd97272250b267e89da498ef1b0
Dayes Camara <dayescamara@gmial.com>
2023-06-27 20:02:45 -0400

    modified:   Develop/script.js

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 var uppercaseChars = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 var lowercaseChars = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-var numberChars = "0,1,2,3,4,5,6,7,8,9";
-var specialChars = "!,@,#,$,%,&,*,^,?";
+var numbersChars = "0,1,2,3,4,5,6,7,8,9";
+var specialChars = "!,@,#,$,%,&,*,^,?,(),{},++,<>,[],{],'{,";
 
 // Write password to the #password input
 function writePassword() {
@@ -11,6 +11,48 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
+}
+
+function generatePassword() {
+    var passwordLength = prompt("Enter a good password that ranges from 8 to 128");
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("The password didn't meet the required length between 8 and 128. Follow the rules, Buddy!")
+        return '';
+    }
+    var includeLowercase = confirm('Add lowercase chracters? it adds more protection!')
+    var includeUppercase = confirm("Add uppercase characters? studys show adding these improves proctection");
+    var includeNumbers= confirm("Add numbers chracters to password?");
+    var includeSpecial = confirm("Do you like to use specials chars in password?");
+
+    
+  var chars = "";
+
+  if (includeLowercase) {
+    chars += lowercaseChars;
+  }
+
+  if (includeUppercase) {
+    chars += uppercaseChars;
+  }
+
+  if (includeNumeric) {
+    chars += numericChars;
+  }
+
+  if (includeSpecial) {
+    chars += specialChars;
+  }
+
+  var password = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
 }
 
 // Add event listener to generate button
